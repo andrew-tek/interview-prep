2ce4c8836c80ecd5f9e424f0fbeced0bf0003bb1
Andrew Tek <andrewtek@gmail.com>
2025-03-11 18:20:42 -0700

    updated solution for combinationSum

--- neetcode-150/src/problems/Backtracking/medium/combinationSums.js
+++ neetcode-150/src/problems/Backtracking/medium/combinationSums.js
@@ -6,23 +6,20 @@ class Solution {
      */
     combinationSum(nums, target) {
         const res = [];
-        nums.sort((a, b) => a - b);
 
-        const dfs = (i, cur, total) => {
-            if(total === target){
+        const dfs = (i, cur, total) =>{
+            if(total == target){
                 res.push([...cur]);
+                return
+            }
+            if(i >= nums.length || total > target){
                 return;
             }
-
-            for(let j = i; j < nums.length; j++){
-                if(total + nums[j] > target){
-                    return
-                }
-                cur.push(nums[j]);
-                dfs(j, cur, total + nums[j]);
-                cur.pop();
-            }
-        };
+            cur.push(nums[i]);
+            dfs(i, cur, total + nums[i]);
+            cur.pop();
+            dfs(i + 1, cur, total);
+        }
 
         dfs(0, [], 0);
         return res;
